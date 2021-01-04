f43621e57d93d612d8f00e8cd2215b99b9715656
Michael Matloka <dev@twixes.com>
2021-01-04 04:36:24 +0100

    Configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        /*'plugin:import/errors', 'plugin:import/warnings',*/ 'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+    ],
+    ignorePatterns: ['dist', 'node_modules'],
+    rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        curly: 'error',
+    },
+}
